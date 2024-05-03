247d34bd8ca3af40eca1e9c951f86b1c3d6b727d
Your Nameecho <you@example.com>
2024-05-03 10:37:02 +0530

    search and email validation done

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ let textAreaEl = registerationForm.querySelector('textarea');
 let tBodyEl = document.querySelector('.t-body');
 let rBtn = document.querySelector('.r-btn');
 let uBtn = document.querySelector('.u-btn');
+let searchEl = document.querySelector('.search');
 
 let usersInfo = [];
+let allTd;
 
 // get data from localstorage and store the data into array
 if (localStorage.getItem('user') !== null) {
@@ -81,71 +83,113 @@ const deleteRegistration = (allDelBtn) => {
 }
 
 // Edit the registration function
+// const editRegistration = (allEditBtn) => {
+//     allEditBtn.forEach((editBtn, index) => {
+//         editBtn.onclick = () => {
+
+//             let tr = editBtn.parentElement.parentElement;
+//             let allTd = tr.querySelectorAll('td');
+
+//             // Second way of updating code using form submission
+
+//             // assign values to form
+//             // allinputs[0].value = allTd[0].innerText;
+//             // allinputs[1].value = allTd[1].innerText;
+//             // allinputs[2].value = allTd[2].innerText;
+//             // allinputs[3].value = allTd[3].innerText;
+//             // textAreaEl.value = allTd[4].innerText;
+
+//             // change form btn from register to update btn
+//             // rBtn.classList.add('d-none');
+//             // uBtn.classList.remove('d-none');
+
+//             // For loop for alltds to update data in a row
+//             for (let i = 0; i < allTd.length - 1; i++) {
+//                 allTd[i].contentEditable = true;
+//                 allTd[i].style.border = '2px solid red';
+//             }
+
+//             // update coding
+//             let updateBtn = allTd[allTd.length - 1].querySelector('button');
+//             updateRegistration(updateBtn, allTd, index);
+//             // updateRegistration(uBtn, allTd, index);
+//         }
+//     })
+// }
+
 const editRegistration = (allEditBtn) => {
     allEditBtn.forEach((editBtn, index) => {
         editBtn.onclick = () => {
-
             let tr = editBtn.parentElement.parentElement;
-            let allTd = tr.querySelectorAll('td');
+            allTd = tr.querySelectorAll('TD');
 
-            // Second way of updating code using form submission
-
-            // assign values to form
-            // allinputs[0].value = allTd[0].innerText;
-            // allinputs[1].value = allTd[1].innerText;
-            // allinputs[2].value = allTd[2].innerText;
-            // allinputs[3].value = allTd[3].innerText;
-            // textAreaEl.value = allTd[4].innerText;
-
-            // change form btn from register to update btn
-            // rBtn.classList.add('d-none');
-            // uBtn.classList.remove('d-none');
-
-            // For loop for alltds to update data in a row
-            for (let i = 0; i < allTd.length - 1; i++) {
-                allTd[i].contentEditable = true;
-                allTd[i].style.border = '2px solid red';
+            for (let i = 0; i < allinputs.length; i++) {
+                allinputs[i].value = allTd[i].innerText
             }
+            textAreaEl.value = allTd[allTd.length - 2].innerText;
+            rBtn.classList.add('d-none');
+            uBtn.classList.remove('d-none');
 
-            // update coding
-            let updateBtn = allTd[allTd.length - 1].querySelector('button');
-            updateRegistration(updateBtn, allTd, index);
-            // updateRegistration(uBtn, allTd, index);
+            // update coding using registration form.
+            updateRegistration(index);
         }
     })
 }
 
-// Update registration
-const updateRegistration = (updateBtn, allTd, index) => {
-    updateBtn.innerHTML = `<i class="fa fa-save"></i>`
-    updateBtn.onclick = () => {
-        let objParam = {
-            name: allTd[0].innerText,
-            email: allTd[1].innerText,
-            password: allTd[2].innerText,
-            fatherName: allTd[3].innerText,
-            address: allTd[4].innerText
+const updateRegistration = (index) => {
+    uBtn.onclick = () => {
+        let updateObj = {
+            name: allinputs[0].value,
+            email: allinputs[1].value,
+            password: allinputs[2].value,
+            fatherName: allinputs[3].value,
+            address: textAreaEl.value
         }
-        // let objParam = {
-        //     name: allinputs[0].value,
-        //     email: allinputs[1].value,
-        //     password: allinputs[2].value,
-        //     fatherName: allinputs[3].value,
-        //     address: textAreaEl.value
-        // }
-        usersInfo[index] = objParam;
+        usersInfo[index] = updateObj;
+        rBtn.classList.remove('d-none');
+        uBtn.classList.add('d-none');
         showInformation(usersInfo);
         localStorage.setItem('user', JSON.stringify(usersInfo));
-        // change form btn from update to register btn
-        // rBtn.classList.remove('d-none');
-        // uBtn.classList.add('d-none');
+        registerationForm.reset();
         Swal.fire({
             title: "Success !",
-            text: "Data Updated Successfully",
+            text: "Data Updated successfully !",
             icon: "success"
         });
     }
 }
+
+// Update registration
+// const updateRegistration = (updateBtn, allTd, index) => {
+//     updateBtn.innerHTML = `<i class="fa fa-save"></i>`
+//     updateBtn.onclick = () => {
+//         let objParam = {
+//             name: allTd[0].innerText,
+//             email: allTd[1].innerText,
+//             password: allTd[2].innerText,
+//             fatherName: allTd[3].innerText,
+//             address: allTd[4].innerText
+//         }
+//         // let objParam = {
+//         //     name: allinputs[0].value,
+//         //     email: allinputs[1].value,
+//         //     password: allinputs[2].value,
+//         //     fatherName: allinputs[3].value,
+//         //     address: textAreaEl.value
+//         // }
+//         usersInfo[index] = objParam;
+//         showInformation(usersInfo);
+//         localStorage.setItem('user', JSON.stringify(usersInfo));
+//         // change form btn from update to register btn
+//         // rBtn.classList.remove('d-none');
+//         // uBtn.classList.add('d-none');
+//         Swal.fire({
+//             title: "Success !",
+//             text: "Data Updated Successfully",
+//             icon: "success"
+//         });
+//     }
+// }
 
 // Call the function when page loads
 showInformation(usersInfo);
@@ -163,20 +207,54 @@ registerationForm.onsubmit = (e) => {
             address: textAreaEl.value
         }
 
-        usersInfo.push(user);
-        localStorage.setItem('user', JSON.stringify(usersInfo));
-        registerationForm.reset();
-        Swal.fire({
-            title: "Success !",
-            text: "Registration Completed",
-            icon: "success"
+        let checkEmail = usersInfo.find((data) => {
+            return data.email == user.email;
         });
-        showInformation(usersInfo);
+
+        if (!checkEmail || checkEmail === undefined) {
+            usersInfo.push(user);
+            localStorage.setItem('user', JSON.stringify(usersInfo));
+            showInformation(usersInfo);
+            registerationForm.reset();
+            Swal.fire({
+                title: "Success !",
+                text: "Registration Completed",
+                icon: "success"
+            });
+        } else {
+            Swal.fire({
+                title: "Warning !",
+                text: "Email already exists, please try again.",
+                icon: "warning"
+            });
+        }
     } else {
         Swal.fire({
             title: "Warning !",
             text: "All fields are mandatory to fill.",
             icon: "warning"
         });
+    }
+}
+
+// Search coding
+searchEl.oninput = () => {
+    searching();
+}
+
+const searching = () => {
+    let sValue = searchEl.value.toLowerCase();
+    let allTr = tBodyEl.querySelectorAll('TR');
+
+    for (let i = 0; i < allTr.length; i++) {
+        let allTd = allTr[i].querySelectorAll('TD');
+        let name = allTd[0].innerText.toLowerCase();
+        let email = allTd[1].innerText.toLowerCase();
+
+        if (name.indexOf(sValue) != -1 || email.indexOf(sValue) != -1) {
+            allTr[i].classList.remove('d-none');
+        } else {
+            allTr[i].classList.add('d-none');
+        }
     }
 }
